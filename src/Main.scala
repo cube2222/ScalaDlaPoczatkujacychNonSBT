c72efd8180fb75781e34200d34a492eca6aaaeb8
Jakub Martin <jakub.wit.martin@gmail.com>
2016-02-10 18:11:56 +0100

    File Main

--- src/Main.scala
+++ src/Main.scala
@@ -7,7 +7,7 @@ import scala.util.parsing.combinator._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    /* Testing FileTree:
+    //Testing FileTree:
     val ft = FileTree.loadFileTree(new io.File("/tmp"))
     val filesAndDirectories = ft.getFilesAndDirectories()
     println("Files:")
@@ -24,11 +24,11 @@ object Main {
     val newFt = FileTree.fileTreeFromStringParser.parseAll(FileTree.fileTreeFromStringParser.obj, ft.toString())
     println(newFt)
     println(newFt.get.getDuplicates())
-    */
 
+/*
     val parsedLogicOperator = LogicalOperator.LogicParser.parseAll(LogicalOperator.LogicParser.obj, "And(And(true, true), Or(false, true), Xor(false,true))")
     println(parsedLogicOperator)
     println(parsedLogicOperator.getOrElse(False).evalWithActors())
-
+*/
   }
 }
